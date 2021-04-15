1713179cf8b34477387241cb6a50e2522422b940
Sebaxtian12 <j.juansebas@gmail.com>
2021-04-15 16:33:44 -0500

    Entity Clientes

---
+++ app/src/main/java/co/edu/unab/castellanos/jose/restaurantfastfood_clientes/Clientes.java
@@ -0,0 +1,33 @@
+package co.edu.unab.castellanos.jose.restaurantfastfood_clientes;
+
+public class Clientes {
+
+    private int id;
+    private String correo;
+    private String password;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
